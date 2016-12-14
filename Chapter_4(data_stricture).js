627378b0896e7930ec558f2d5e29bda44c3a3ca6
Kiran Mahale <kiranmahale9@gmail.com>
2016-12-14 23:05:42 +0530

    Much validated Range() function

--- Chapter_4(data_stricture).js
+++ Chapter_4(data_stricture).js
@@ -1,8 +1,23 @@
 //First exercise
-function Range(start, end){
+function Range(start , end, step){
+	if(isNaN(start) || isNaN(end) || isNaN(step) ){ return [];}
+	if (step == undefined) {step = 1;}
 	var arr = [];
-	for(var i = start; i <= end; i++ ){
-		arr.push(i);
+	if(step > 0){
+		for(var i = start; i <= end; i = i+step ){
+			arr.push(i);
+		}
+	}else if(step < 0){
+		for(var i = end; i >= start; i = i + step ){
+			arr.push(i);
+		}
 	}
 	return arr;
+}
+function sum(arr){
+	var total = 0;
+	for(var i = 0; i < arr.length; i++){
+		total += arr[i];
+	}
+	return total;
 }
