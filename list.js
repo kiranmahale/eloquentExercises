479c89460470d1017524c578ed39fbf915f8ba87
Kiran Mahale <kiranmahale9@gmail.com>
2016-12-17 23:38:34 +0530

    Upadting code for recursive nth function

--- list.js
+++ list.js
@@ -1,4 +1,4 @@
-function arraToList(arr){
+function arrayToList(arr){
 	var list = null;
 	for(var i = arr.length -1; i >= 0; i--){
 		list = {value: arr[i], rest: list};
@@ -11,5 +11,22 @@ function listToarray(list){
 	for(var node=list; node; node = node.rest){
 		arr.push(node.value);
 	}
-	return arr;
+	return arr;	
+}
+
+function nth(n, list){
+	for(var node = list; node; node = node.rest ){
+		n--;
+		if(n == 0){
+		return node.value;
+	}
+	}
+}
+function rec_nth(n,list){
+	debugger;
+	if(n==1){
+		return list.value;
+	}else{
+		rec_nth(n = n-1,list.rest);
+	}
 }
