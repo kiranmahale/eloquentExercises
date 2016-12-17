cbe422ef78b88145a4e4d1fde2d0e44e4f03c317
Kiran Mahale <kiranmahale9@gmail.com>
2016-12-17 23:11:59 +0530

    Updating code for list and array function

--- list.js
+++ list.js
@@ -1,11 +1,15 @@
 function arraToList(arr){
 	var list = null;
-	for(var i = arr.length; i >= 0; i--){
+	for(var i = arr.length -1; i >= 0; i--){
 		list = {value: arr[i], rest: list};
 	}
 	return list;
 }
 
-function listToarray(){
-	
+function listToarray(list){
+	var arr = [];
+	for(var node=list; node; node = node.rest){
+		arr.push(node.value);
+	}
+	return arr;
 }
