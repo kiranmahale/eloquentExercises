5e448e8a01ee66128b87ae1d7441ab8ad3fc9ab5
Kiran Mahale <kiranmahale9@gmail.com>
2017-05-24 23:08:57 +0530

    Updating exercises

--- Chapter_3(functions).js
+++ Chapter_3(functions).js
@@ -1,3 +1,13 @@
+//We can declare a function in pure javascript like
+var power = function(base,exponent){
+	var result = 1;
+	for(var count = 0; count < exponent; count++){
+		result *= base;
+	}
+	return result;
+}
+
+//*************************************Exercise******************************************
 //will count the no of b's in a string
 function countBs(str){
 	var count = 0;
